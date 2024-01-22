0a715cf692fca675d1ad2aa6587fdba457392f35
Arif Momin <arifmominweb@gmail.com>
2024-01-22 23:28:35 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// ===================banner slider ==================
+$('.banner_img').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplaySpeed: 2000,
+    prevArrow:'<i class="fa-solid fa-angle-left prev"></i>',
+    nextArrow:'<i class="fa-solid fa-angle-right next"></i>',
+  });
